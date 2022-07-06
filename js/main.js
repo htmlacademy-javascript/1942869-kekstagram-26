d5043a05bf928b054e5674c253db1704d8ba8223
olesya-syrtina <97554256+olesya-syrtina@users.noreply.github.com>
2022-07-06 22:14:35 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -33,3 +33,72 @@ getRandomIntInclusive(1, 10);
 maxStrLength('Строка', 10);
 // console.log(maxStrLength('Строка', 10));
 // console.log(maxStrLength('Строка', 1));
+
+/* Код проверки функций*/
+
+const NAMES = [
+  'Артём',
+  'Никита',
+  'Дима',
+  'Олеся',
+  'Таня',
+  'Юля',
+  'Лена',
+  'Аня'
+];
+
+const DESCRIPTIONS = [
+  'Я',
+  'Ещё раз я',
+  'Снова я?',
+  'Опять я',
+  'Я любимый'
+];
+
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const getRandomEnumElement = (elems) => elems[getRandomIntInclusive(0,elems.length-1)];
+
+let commentId = 1;
+
+const makeComment = () => ({
+  id: commentId++,
+  avatar:`img/avatar-${  getRandomIntInclusive(1, 6)  }.svg`,
+  message: getRandomEnumElement(COMMENTS),
+  name: getRandomEnumElement(NAMES)
+});
+
+const makeComments = (count) => {
+  const comments = [];
+  for (let i=1; i <= count; i++) {
+    comments.push(makeComment(i));
+  }
+
+  return comments;
+};
+
+const makePhoto = (id) => ({
+  id: id,
+  url: `photos/${  id  }.jpg`,
+  description: getRandomEnumElement(DESCRIPTIONS),
+  likes: getRandomIntInclusive(25,100),
+  comments: makeComments(getRandomIntInclusive(0,5))
+});
+
+const makePhotos = (count) => {
+  const photo = [];
+  for (let i=1; i <= count; i++) {
+    photo.push(makePhoto(i));
+  }
+
+  return photo;
+};
+
+makePhotos(25);
